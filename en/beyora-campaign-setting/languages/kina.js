958916efac595a26b9d1af85b1e363b8cc671d20
treeman0013 <connorwillchris@gmail.com>
2025-01-08 12:31:49 -0700

    nother commit

--- en/beyora-campaign-setting/languages/kina.js
+++ en/beyora-campaign-setting/languages/kina.js
@@ -1,1 +1,16 @@
 const lexiconTable = document.getElementById('lexiconTable');
+
+fetch('./kina-dictionary.json')
+    .then((response) => response.json())
+    .then((json) => {
+        for (let i = 0; i < json.length; i++) {
+            lexiconTable.innerHTML += `
+            <tr>
+                <td>${json[i].word}</td>
+                <td>${json[i].pos}</td>
+                <td>${json[i].meaning}</td>
+            </tr>
+            `;
+        }
+    })
+    ;

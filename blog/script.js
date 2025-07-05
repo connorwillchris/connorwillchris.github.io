7b8208c79b632efeada68987bb322c194a4a016f
connorwillchris <connorwillchris@gmail.com>
2025-07-05 14:04:46 -0600

    more changes, updates to blog

--- blog/script.js
+++ blog/script.js
@@ -2,14 +2,9 @@ const postList = document.getElementById('postList');
 
 let postArray = [
     '2025-7-4-my-first-post',
+    //'2025-7-5-quick-update',
 ];
 
 for (let i = 0; i < postArray.length; i++) {
-    postList.innerHTML += `
-    <li>
-        <a href="/blog/posts/${postArray[i]}.html">
-        ${postArray[i]}
-        </a>
-    </li>
-    `;
+    postList.innerHTML += `<li><a href="/blog/posts/${postArray[i]}.html">${postArray[i]}</a></li>`;
 }

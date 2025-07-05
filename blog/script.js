ca560bdcf4cd13354a312a03705e900a01367cc9
connorwillchris <connorwillchris@gmail.com>
2025-07-05 16:10:52 -0600

    quick update blog post

--- blog/script.js
+++ blog/script.js
@@ -2,7 +2,7 @@ const postList = document.getElementById('postList');
 
 let postArray = [
     '2025-7-4-my-first-post',
-    //'2025-7-5-quick-update',
+    '2025-7-5-quick-update',
 ];
 
 for (let i = 0; i < postArray.length; i++) {

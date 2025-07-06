92bb62259eee8d0523e76a9ca4a79a502c77f620
connorwillchris <connorwillchris@gmail.com>
2025-07-06 11:51:08 -0600

    more changes.

--- projects/world-of-dungeons/script.js
+++ projects/world-of-dungeons/script.js
@@ -5,6 +5,10 @@ const characterCreationHeader = document.getElementById('characterCreationHeader
 const attributesParagraph = document.getElementById('attributesParagraph');
 const hitDiceParagraph = document.getElementById('hitDiceParagraph');
 
+const fighterClassParagraph = document.getElementById('fighterClassParagraph');
+
+/*console.log(fighterClassParagraph);*/
+
 // hack to use English by default.
 let lang = 'tok';
 
@@ -13,10 +17,10 @@ const englishJson = {
     worldOfDungeonsTitle: 'World of Dungeons',
     rulesSummaryHeader: 'Rules Summary',
     characterCreationHeader: 'Character Creation',
-
-    // character creation
     attributesParagraph: '<b>Attributes:</b> Roll 2d6 for each. On a 6-, the value is +0. On a 7-9, the value is +1. On a 10-11, the value is +2. On a 12, the value is +3.',
     hitDiceParagraph: '<b>You have 1 Hit Die (d6) + extra hit dice equal to your CON.</b> Roll all your hit dice and keep a number equal to your level to determine your HP. When you rest and consume a ration/waterskin/wineskin, you may re-roll your HP. If you are attended by a healer, roll an extra hit dice.',
+
+    fighterClassParagraph: '<b>FIGHTERS</b> get Athletics. Choose two special abilities: <b>Skirmish</b> (+1 damage and worn armor counts as one type lighter), <b>Tough</b> (+1 armor), <b>Slay</b> (+2 melee damage), <b>Hardy</b> (+6 HP).'
 };
 
 const tokJson = {
@@ -24,10 +28,10 @@ const tokJson = {
     worldOfDungeonsTitle: 'ma pi tomo anpa',
     rulesSummaryHeader: 'nasin ali',
     characterCreationHeader: 'o pali e jan musi',
-
-    // character creation
     attributesParagraph: '<b>nanpa musi:</b> o kepeken e 2k6 tawa nanpa musi ali. sina jo e nanpa pi suli 6 anu lili la, nanpa musi li +0. sina jo e nanpa pi suli 7 anu nanpa pi 10 lili la, nanpa musi li +1.',
     hitDiceParagraph: '<b>sina jo e leko sijelo wan la, sijelo sina li pana e leko sijelo sin.</b> o kepeken e leko nanpa ali.',
+
+    fighterClassParagraph: '<b>jan utala</b> jo e ken pali. o wile e ',
 };
 
 const click = function() {
@@ -40,6 +44,8 @@ const click = function() {
         characterCreationHeader.innerHTML = tokJson.characterCreationHeader;
         attributesParagraph.innerHTML = tokJson.attributesParagraph;
         hitDiceParagraph.innerHTML = tokJson.hitDiceParagraph;
+
+        fighterClassParagraph.innerHTML = tokJson.fighterClassParagraph;
     }
     else if (lang === 'tok') {
         lang = 'en';
@@ -50,6 +56,8 @@ const click = function() {
         characterCreationHeader.innerHTML = englishJson.characterCreationHeader;
         attributesParagraph.innerHTML = englishJson.attributesParagraph;
         hitDiceParagraph.innerHTML = englishJson.hitDiceParagraph;
+
+        fighterClassParagraph.innerHTML = englishJson.fighterClassParagraph;
     }
 };
 
